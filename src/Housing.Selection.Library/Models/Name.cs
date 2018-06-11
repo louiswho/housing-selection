f60e335e3d9899dda626e9bd1d1bedd190d4b42d
Rafael Berger <Rafiberger613@gmail.com>
2018-06-11 10:34:04 -0400

    Modified the name class comments. [#158039179]

--- src/Housing.Selection.Library/Models/Name.cs
+++ src/Housing.Selection.Library/Models/Name.cs
@@ -4,26 +4,27 @@ namespace Housing.Selection.Library.Models
 {
     /// <summary>
     /// The Name class.
-    /// Contains the properties of Revature  associate Names.
+    /// Contains the properties of Revature associate Names.
     /// </summary>
    public  class Name
     {
-        /// <summary>The Id property represents the Primary Key  of the associate  for the Housing-Selection database Table .</summary>
+        /// <summary>The Id property represents the Primary Key of the associate  for the Housing-Selection database Table.</summary>
         /// <value>The Id property gets/sets the value of the Guid field, Id.</value>
-
         public Guid Id { get; set; }
 
-        /// <summary>The NameId property represents the Primary Key  of the associate  for the Housing-Selection database Table .</summary>
-        /// <value>The NameId property gets/sets the value of the Guid field, GuidId.</value>
-
+        /// <summary>The NameId property represents the Primary Key  of the associate  for the Housing-Selection database Table.</summary>
+        /// <value>The NameId property gets/sets the value of the Guid field, NameId.</value>
         public Guid NameId { get; set; }
 
         /// <summary>The First  property represents the first name of the associate.</summary>
         /// <value>The first property gets/sets the value of the string field, first.</value>
         public string First { get; set; }
+
+        /// <summary>The Middle property represents the middle name of the associate.</summary>
+        /// <value>The middle property gets/sets the value of the string field, first.</value>
         public string Middle { get; set; }
 
-        /// <summary>The Last  property represents the last name of the associate.</summary>
+        /// <summary>The middle property represents the last name of the associate.</summary>
         /// <value>The last property gets/sets the value of the string field, last.</value>
         public string Last { get; set; }
     }
