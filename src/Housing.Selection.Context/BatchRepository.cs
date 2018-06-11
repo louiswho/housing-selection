2071d80dffd5137a5b68824d57df8fdaf682a723
Rafael Berger <Rafiberger613@gmail.com>
2018-06-11 15:31:21 -0400

    Add Repos. [#158039176]

---
+++ src/Housing.Selection.Context/BatchRepository.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Housing.Selection.Context.Interfaces;
+using Housing.Selection.Library;
+
+namespace Housing.Selection.Context.Repositories
+{
+    /// <summary>
+    /// Add, read, update and get by Id Revature batches from Housing-Selection database.
+    /// </summary>
+    
+
+    public class BatchRepository : IBatchRepository
+    {
+        private List<Batch> batches = new List<Batch>();
+
+        public BatchRepository()
+        {
+          
+
+        }
+
+        public void AddBatch(Batch batch)
+        {
+            batches.Add(batch);
+           
+        }
+
+        public IEnumerable<Batch> GetBatches()
+        {
+
+            return batches;
+        }
+
+        public Batch GetBatchById(Guid id)
+        {
+            return batches.First(x => x.BatchId == id);
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+     
+    }
+}
---
+++ src/Housing.Selection.Context/RoomRepository.cs
@@ -0,0 +1,46 @@
+﻿using Housing.Selection.Context.Interfaces;
+using Housing.Selection.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace Housing.Selection.Context.Repositories
+{
+
+    /// <summary>
+    /// Create, read, update and get by Id Revature rooms from Housing-Selection database.
+    /// </summary>
+    /// 
+
+
+   public class RoomRepository : IRoomRepository
+    {
+
+        private List<Room> rooms = new List<Room>();
+
+        public RoomRepository()
+        {
+           
+
+        }
+        public void AddRoom(Room room)
+        {
+            rooms.Add(room);
+        }
+
+        public Room GetRoomById(Guid id)
+        {
+            return rooms.First(x => x.RoomId == id);
+        }
+
+        public IEnumerable<Room> GetRooms()
+        {
+            return rooms;
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
