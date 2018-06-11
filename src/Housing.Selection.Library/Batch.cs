ba8ead58893a92e5d955ba5aa186f7f0b585f80e
jalmblade <j.almblade@gmail.com>
2018-06-11 10:17:39 -0400

    Added Batch model. [#158039179]

---
+++ src/Housing.Selection.Library/Batch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing.Selection.Library
+{
+    public class Batch
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid BatchId { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        [StringLength(50)]
+        public string BatchName { get; set; }
+        [Range(0, 100)]
+        public int BatchOccupancy { get; set; }
+        [StringLength(50)]
+        public string BatchSkill { get; set; }
+
+        public ICollection<User> Users { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
