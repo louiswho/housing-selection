0e3427c2ba6793c3610215adb044d7575cd85c07
Rafael Berger <Rafiberger613@gmail.com>
2018-06-11 10:52:38 -0400

    Added user collection to Name. [#158039179]

--- src/Housing.Selection.Library/Models/Name.cs
+++ src/Housing.Selection.Library/Models/Name.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Housing.Selection.Library.Models
 {
@@ -10,14 +12,17 @@ namespace Housing.Selection.Library.Models
     {
         /// <summary>The Id property represents the Primary Key of the associate  for the Housing-Selection database Table.</summary>
         /// <value>The Id property gets/sets the value of the Guid field, Id.</value>
+        [Key]
         public Guid Id { get; set; }
 
         /// <summary>The NameId property represents the Primary Key  of the associate  for the ServiceHub database Table.</summary>
         /// <value>The NameId property gets/sets the value of the Guid field, NameId.</value>
+        [Required]
         public Guid NameId { get; set; }
 
         /// <summary>The First  property represents the first name of the associate.</summary>
         /// <value>The first property gets/sets the value of the string field, first.</value>
+        [Required]
         public string First { get; set; }
 
         /// <summary>The Middle property represents the middle name of the associate.</summary>
@@ -26,6 +31,11 @@ namespace Housing.Selection.Library.Models
 
         /// <summary>The middle property represents the last name of the associate.</summary>
         /// <value>The last property gets/sets the value of the string field, last.</value>
+        [Required]
         public string Last { get; set; }
+
+        /// <summary>The Users property represents a collection of all Revature associates.</summary>
+        /// <value>The Users property gets/sets the value of the Collection of User objects.</value>
+        public ICollection<User> Users { get; set; }
     }
 }
