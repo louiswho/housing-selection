84e9d91c8e47411bbdb45ecc3781330a4ee62388
Unknown <mrcrozier@gmail.com>
2018-06-11 11:02:25 -0400

    Changed Address model to reflect the wiki [#158039179]

--- src/Housing.Selection.Library/Address.cs
+++ src/Housing.Selection.Library/Address.cs
@@ -7,20 +7,29 @@ namespace Housing.Selection.Library
 {
     public class Address
     {
-        [Required]
-        public Guid Id { get; set; }
-        [Required]
-        public Guid AddressId { get; set; }
-        [Required]
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        [Required]
-        public string City { get; set; }
-        [Required]
-        public string State { get; set; }
-        [Required]
-        public string PostalCode { get; set; }
-        [Required]
-        public string Country { get; set; }
+    [Required]
+    public Guid Id { get; set; }
+    [Required]
+    public Guid AddressId { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string Address1 { get; set; }
+    public string Address2 { get; set; }
+    [Required]
+    [StringLength(25)]
+    public string City { get; set; }
+    [Required]
+    [StringLength(2, MinimumLength=2)]
+    public string State { get; set; }
+    [Required]
+    [StringLength(5, MinimumLength=5)]
+    public string PostalCode { get; set; }
+    [Required]
+    [StringLength(2, MinimumLength=2)]
+    public string Country { get; set; }
+
+    public ICollection<Batch> Batches { get; set; }
+    public ICollection<User> Users { get; set; }
+    public ICollection<Room> Rooms { get; set; }
     }
 }
