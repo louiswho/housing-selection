37ccae80aba94f0a98ccfd3244efb1d7784719d3
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-11 16:50:55 -0400

    Added Interface, Add/Remove User to/from room view model, custom room search model, and the ISelectionService interface

---
+++ src/Housing.Selection.Context/ISelectionService.cs
@@ -0,0 +1,13 @@
+﻿using Housing.Selection.Library;
+using System.Collections.Generic;
+
+namespace Housing.Selection.Context
+{
+    interface ISelectionService
+    {
+        List<Batch> GetBatches();
+        List<Room> GetRooms();
+        List<User> GetUsers();
+        List<Room> CustomSearch(RoomSearchViewModel roomSearchViewModel);
+    }
+}
---
+++ src/Housing.Selection.Library/RoomSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Housing.Selection.Library
+{
+    class RoomSearchViewModel
+    {
+        public string Location { get; set; }
+        public string Batch { get; set; }
+        public int BatchMinimumPercentage { get; set; }
+        public char Gender { get; set; }
+        public bool IsCompletelyUnassigned { get; set; }
+    }
+}
