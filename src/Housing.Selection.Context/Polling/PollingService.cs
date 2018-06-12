990bcb26e6b0222da5a59de3a28e5bf3b7edf94b
Unknown <mrcrozier@gmail.com>
2018-06-11 23:24:48 -0400

    Added room update call to polling [#158039176]

--- src/Housing.Selection.Context/Polling/PollingService.cs
+++ src/Housing.Selection.Context/Polling/PollingService.cs
@@ -11,10 +11,12 @@ namespace Housing.Selection.Context.Polling
     {
         private IHttpClientWrapper httpClient;
         private IBatchRepository batchRepository;
-        public PollingService(IHttpClientWrapper httpClient, IBatchRepository batchRepository)
+        private IRoomRepository roomRepository;
+        public PollingService(IHttpClientWrapper httpClient, IBatchRepository batchRepository, IRoomRepository roomRepository)
         {
             this.httpClient = httpClient;
             this.batchRepository = batchRepository;
+            this.roomRepository = roomRepository;
         }
         /// <summary>
         ///  Poll all Service Hub databases through their respective API's and updates our databases with the new data 
@@ -27,13 +29,24 @@ namespace Housing.Selection.Context.Polling
                 UpdateBatch(batch);
             }
             var rooms = httpClient.GetRooms();
+            foreach (var room in rooms)
+            {
+                UpdateRoom(room);
+            }
             var users = httpClient.GetUsers();
         }
         private void UpdateBatch(ApiBatch batch)
-        {
-            
-            var housingBatch = batchRepository.GetByBatchId(batch.BatchId);
-            //TODO: Finish writing this method
+        {            
+            var housingBatch = batchRepository.GetBatchByBatchId(batch.BatchId);
+            housingBatch = housingBatch.ConvertFromServiceModel(apiBatch: batch);
+            batchRepository.UpdateBatch(housingBatch);            
+        }
+
+        private void UpdateRoom(ApiRoom room)
+        {            
+            var housingRoom = roomRepository.GetRoomByRoomId(room.RoomId);
+            housingRoom = housingRoom.ConvertFromServiceModel(apiBatch: batch);
+            roomRepository.UpdateRoom(housingRoom);            
         }
     }
 }
