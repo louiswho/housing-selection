d40835094b2f78871cc6bf096dfec6ff68c38774
Unknown <mrcrozier@gmail.com>
2018-06-12 09:29:36 -0400

    Added comments, update user method to polling  [#158039176]

--- src/Housing.Selection.Context/Polling/PollingService.cs
+++ src/Housing.Selection.Context/Polling/PollingService.cs
@@ -12,14 +12,16 @@ namespace Housing.Selection.Context.Polling
         private IHttpClientWrapper httpClient;
         private IBatchRepository batchRepository;
         private IRoomRepository roomRepository;
-        public PollingService(IHttpClientWrapper httpClient, IBatchRepository batchRepository, IRoomRepository roomRepository)
+        private IUserRepository userRepository;
+        public PollingService(IHttpClientWrapper httpClient, IBatchRepository batchRepository, IRoomRepository roomRepository, IUserRepository userRepository)
         {
             this.httpClient = httpClient;
             this.batchRepository = batchRepository;
             this.roomRepository = roomRepository;
+            this.userRepository = userRepository;
         }
         /// <summary>
-        ///  Poll all Service Hub databases through their respective API's and updates our databases with the new data 
+        ///  Poll all Service Hub databases through their respective API's and updates our database with the new data 
         /// </summary>
         public void PollAll()
         {
@@ -34,19 +36,43 @@ namespace Housing.Selection.Context.Polling
                 UpdateRoom(room);
             }
             var users = httpClient.GetUsers();
+            foreach (var user in users)
+            {
+                UpdateUser(user);
+            }
         }
+        
+        /// <summary>
+        ///  Get batch by batchId (taken from service hubs api call)
+        ///  then convert the service hub model to housing model and update our database with the new information 
+        /// </summary>
         private void UpdateBatch(ApiBatch batch)
-        {            
+        {
             var housingBatch = batchRepository.GetBatchByBatchId(batch.BatchId);
             housingBatch = housingBatch.ConvertFromServiceModel(apiBatch: batch);
-            batchRepository.UpdateBatch(housingBatch);            
+            batchRepository.UpdateBatch(housingBatch); //TODO: Fix this method call when the function gets added
         }
 
+        /// <summary>
+        ///  Get room by roomId (taken from service hubs api call)
+        ///  then convert the service hub model to housing model and update our database with the new information 
+        /// </summary>
         private void UpdateRoom(ApiRoom room)
-        {            
+        {
             var housingRoom = roomRepository.GetRoomByRoomId(room.RoomId);
-            housingRoom = housingRoom.ConvertFromServiceModel(apiBatch: batch);
-            roomRepository.UpdateRoom(housingRoom);            
+            housingRoom = housingRoom.ConvertFromServiceModel(apiRoom: room);
+            roomRepository.UpdateRoom(housingRoom); //TODO: Fix this method call when the function gets added
+        }
+
+        /// <summary>
+        ///  Get user by userId (taken from service hubs api call)
+        ///  then convert the service hub model to housing model and update our database with the new information 
+        /// </summary>
+        private void UpdateUser(ApiUser user)
+        {
+            var housingUser = userRepository.GetUserByUserId(user.UserId);
+            housingUser = housingUser.ConvertFromServiceModel(apiUser: user);
+            userRepository.UpdateUser(housingUser); //TODO: Fix this method call when the function gets added
         }
     }
 }
