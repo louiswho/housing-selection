d4cbba87f01fa15c0d9735f8e7d016da12f38760
jalmblade <j.almblade@gmail.com>
2018-06-12 10:03:44 -0400

    Added ServiceBatchRetrieval implementation and tests for success and fail responses. [#158039176]

--- src/Housing.Selection.Context/HttpRequests/ServiceBatchRetrieval.cs
+++ src/Housing.Selection.Context/HttpRequests/ServiceBatchRetrieval.cs
@@ -11,29 +11,75 @@ namespace Housing.Selection.Context.HttpRequests
         public IHttpClientWrapper Client { get; set; }
         public IApiPathBuilder ApiPath { get; set; }
 
-        public ServiceBatchRetrieval (IHttpClientWrapper httpClient, IApiPathBuilder apiPath)
+        /// <summary>
+        /// This is the constructor, where the HttpClient is injected.
+        /// </summary>
+        /// <remarks>
+        /// For IApiPathBuilder, you only need to pass a new ApiPathBuilder object.
+        /// If any changes to paths need to be made, do it in the ApiPathBuilder base class.
+        /// </remarks>
+        public ServiceBatchRetrieval(IHttpClientWrapper httpClient, IApiPathBuilder apiPath)
         {
             Client = httpClient;
             ApiPath = apiPath;
         }
 
+        /// <summary>
+        /// Asynchronously retrieves all service hub batches.
+        /// </summary>
+        /// <returns>
+        /// Returns a List<ApiBatch>.
+        /// </returns>
         public async Task<List<ApiBatch>> RetrieveAllBatchesAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                List<ApiBatch> batches = new List<ApiBatch>();
+                HttpResponseMessage response = await Client.GetAsync(ApiPath.GetBatchServicePath());
+                if (response.IsSuccessStatusCode)
+                {
+                    batches = await response.Content.ReadAsAsync<List<ApiBatch>>();
+                    return batches;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
 
+        /// <summary>
+        /// Asynchronously retrieves an ApiBatch by id.
+        /// </summary>
+        /// <returns>
+        /// Returns an ApiBatch if found, null if not found.
+        /// </returns>
         public async Task<ApiBatch> RetrieveBatchAsync(Guid guid)
         {
             try
             {
                 ApiBatch batch = new ApiBatch();
 
-                HttpResponseMessage message = await Client.GetAsync(ApiPath.GetBatchServicePath());
-            } catch (Exception ex)
+                HttpResponseMessage response = await Client.GetAsync(ApiPath.GetBatchServicePath(guid));
+                if (response.IsSuccessStatusCode)
+                {
+                    batch = await response.Content.ReadAsAsync<ApiBatch>();
+                    return batch;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
             {
                 throw ex;
             }
-            throw new NotImplementedException();
         }
     }
 }
---
+++ src/Housing.Selection.Testing/Context/HttpRequestsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using System.Net.Http;
+using Housing.Selection.Context.HttpRequests;
+using System.Threading.Tasks;
+using Housing.Selection.Library.ServiceHubModels;
+
+namespace Housing.Selection.Testing.Context
+{
+    public class HttpRequestsTests
+    {
+        [Fact]
+        public async Task GetAllBatches_TestingSuccess_ReturnsApiBatches()
+        {
+            // Arrange
+            List<ApiBatch> apibatchesexpected = new List<ApiBatch>();
+
+            var mockHttpResponse = new Mock<HttpResponseMessage>();
+            mockHttpResponse.Setup(x => x.Content.ReadAsAsync<List<ApiBatch>>()).ReturnsAsync(apibatchesexpected);
+            mockHttpResponse.Setup(x => x.IsSuccessStatusCode).Returns(true);
+
+            var mockIHttpClient = new Mock<IHttpClientWrapper>();
+            mockIHttpClient.Setup(x => x.GetAsync(It.IsAny<String>())).ReturnsAsync(mockHttpResponse.Object);
+
+            var mockApiPath = new Mock<ApiPathBuilder>();
+            mockApiPath.Setup(x => x.GetBatchServicePath()).Returns("string");
+
+            ServiceBatchRetrieval sbr = new ServiceBatchRetrieval(mockIHttpClient.Object, mockApiPath.Object);
+
+            var actual = await sbr.RetrieveAllBatchesAsync();
+
+            Assert.Equal(apibatchesexpected, actual);
+        }
+
+        [Fact]
+        public async Task GetAllBatches_TestingFail_ReturnsNull()
+        {
+            // Arrange
+            List<ApiBatch> apibatchesexpected = new List<ApiBatch>();
+
+            var mockHttpResponse = new Mock<HttpResponseMessage>();
+            mockHttpResponse.Setup(x => x.Content.ReadAsAsync<List<ApiBatch>>()).ReturnsAsync(apibatchesexpected);
+            mockHttpResponse.Setup(x => x.IsSuccessStatusCode).Returns(false);
+
+            var mockIHttpClient = new Mock<IHttpClientWrapper>();
+            mockIHttpClient.Setup(x => x.GetAsync(It.IsAny<String>())).ReturnsAsync(mockHttpResponse.Object);
+
+            var mockApiPath = new Mock<ApiPathBuilder>();
+            mockApiPath.Setup(x => x.GetBatchServicePath()).Returns("string");
+
+            ServiceBatchRetrieval sbr = new ServiceBatchRetrieval(mockIHttpClient.Object, mockApiPath.Object);
+
+            var actual = await sbr.RetrieveAllBatchesAsync();
+
+            Assert.Null(actual);
+        }
+    }
+}
