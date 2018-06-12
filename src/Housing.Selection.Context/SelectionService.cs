551901588fed91f9e6495a1cbd7f0cdc61bdf551
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-12 12:38:52 -0400

    Added logic to SelectionService, Made AddRemove model public, Added missing functionality to ISelectionService
    Added AFilter for custom room search, removed unnecessary using statements from repository interfaces

--- src/Housing.Selection.Context/SelectionService.cs
+++ src/Housing.Selection.Context/SelectionService.cs
@@ -1,41 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Housing.Selection.Library;
-using Housing.Selection.Library.HousingModels;
 
 namespace Housing.Selection.Context
 {
-    public class SelectionService : ISelectionService
+    class SelectionService
     {
-        public AddRemoveUserFromRoomModel AddUserToRoom(AddRemoveUserFromRoomModel addRemoveUserFromRoomModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Room> CustomSearch(RoomSearchViewModel roomSearchViewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Batch> GetBatches()
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Room> GetRooms()
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<User> GetUsers()
-        {
-            throw new NotImplementedException();
-        }
-
-        public AddRemoveUserFromRoomModel RemoveUserFromRoom(AddRemoveUserFromRoomModel addRemoveUserFromRoomModel)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
