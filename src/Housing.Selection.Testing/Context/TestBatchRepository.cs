159db16d63486262bfb3ca380371cd3689aa6d62
Rafael Berger <Rafiberger613@gmail.com>
2018-06-13 12:13:01 -0400

    Get rid of unnecessary using and comment. [#158039176]

--- src/Housing.Selection.Testing/Context/TestBatchRepository.cs
+++ src/Housing.Selection.Testing/Context/TestBatchRepository.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
-//Clean up white space.
+
+
 namespace Housing.Selection.Testing.Context
 {
     public class TestBatchRepository
