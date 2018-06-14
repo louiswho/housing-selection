48df6638872c7a8aff5abea7a6ad139082e880cc
Rafael Berger <Rafiberger613@gmail.com>
2018-06-14 12:01:09 -0400

    Add  Name and Address Repos with Tests. [#158039176]

---
+++ src/Housing.Selection.Context/DataAccess/AddressRepository.cs
@@ -0,0 +1,42 @@
+﻿using Housing.Selection.Library.HousingModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Housing.Selection.Context.DataAccess
+{
+   public class AddressRepository : IAddressRepository
+    {
+        private readonly IDbContext _housingSelectionDbContext;
+
+        public AddressRepository(IDbContext housingSelectionContext)
+        {
+            _housingSelectionDbContext = housingSelectionContext;
+        }
+
+        public void AddAddress(Address address)
+        {
+            _housingSelectionDbContext.Addresses.Add(address);
+        }
+
+        public IEnumerable<Address> GetAddresses()
+        {
+            return _housingSelectionDbContext.Addresses;
+        }
+
+        public Address GetAddressById(Guid id)
+        {
+            return _housingSelectionDbContext.Addresses.First(x => x.Id == id);
+        }
+
+        public Address GetAddressByAddressId(Guid addressId)
+        {
+            return _housingSelectionDbContext.Addresses.First(x => x.AddressId == addressId);
+        }
+
+        public void SaveChanges()
+        {
+            _housingSelectionDbContext.saveChanges();
+        }
+    }
+}
