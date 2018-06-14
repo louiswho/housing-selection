e8823c8acc3ea65e3f2bb8a44097766a3a37c5e1
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-14 14:38:01 -0400

    Added View Models and Mapping, fixed selection controller[#158039184]

---
+++ src/Housing.Selection.Library/ViewModels/NameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Housing.Selection.Library.ViewModels
+{
+    public class NameViewModel
+    {
+        public Guid id { get; set; }
+        
+        public string first { get; set; }
+
+        public string middle { get; set; }
+        
+        public string last { get; set; }
+    }
+}
---
+++ src/Housing.Selection.Library/ViewModels/RoomViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing.Selection.Library.ViewModels
+{
+    public class RoomViewModel
+    {
+        public Guid id { get; set; }
+        
+        public string location { get; set; }
+        
+        public int vacancy { get; set; }
+        
+        public int occupancy { get; set; }
+        
+        public string gender { get; set; }
+        
+        public AddressViewModel address { get; set; }
+
+        public ICollection<UserViewModel> users { get; set; }
+    }
+}
