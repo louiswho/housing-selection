e7a3c8074b4cb69733fbea452a6010eb88346380
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-14 14:47:21 -0400

    Fixed SelectionController to use Mapper

--- src/Housing.Selection.Service/Controllers/SelectionController.cs
+++ src/Housing.Selection.Service/Controllers/SelectionController.cs
@@ -1,7 +1,9 @@
-﻿using Housing.Selection.Context.Selection;
+﻿using AutoMapper;
+using Housing.Selection.Context.Selection;
+using Housing.Selection.Library.HousingModels;
 using Housing.Selection.Library.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
 
 namespace Housing.Selection.Service.Controllers
 {
@@ -10,21 +12,29 @@ namespace Housing.Selection.Service.Controllers
     public class SelectionController : Controller
     {
         private readonly ISelectionService _selection;
+        private readonly IMapper _mapper;
 
-        public SelectionController(ISelectionService selection)
+        public SelectionController(ISelectionService selection, IMapper mapper)
         {
             _selection = selection;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public IActionResult GetBatches()
         {
-            return Ok(_selection.GetBatches());
+            var batches = _selection.GetBatches();
+            var viewModel = _mapper.Map<IEnumerable<Batch>>(batches);
+
+            return Ok(viewModel);
         }
 
         [HttpGet]
         public IActionResult GetRooms()
         {
+            var rooms = _selection.GetRooms();
+            var viewModel = _mapper.Map<IEnumerable<Room>>(rooms);
+
             return Ok(_selection.GetRooms());
         }
 
@@ -32,6 +42,9 @@ namespace Housing.Selection.Service.Controllers
         public IActionResult CustomSearch(RoomSearchViewModel roomSearchViewModel)
         {
             if(!ModelState.IsValid) { return BadRequest(); };
+
+            var rooms = _selection.CustomSearch(roomSearchViewModel);
+            var viewModel = _mapper.Map<IEnumerable<Room>>(rooms);
 
             return Ok(_selection.CustomSearch(roomSearchViewModel));
         }
--- src/Housing.Selection.Testing/Service/SelectionControllerTests.cs
+++ src/Housing.Selection.Testing/Service/SelectionControllerTests.cs
@@ -4,9 +4,7 @@ using Housing.Selection.Library.ViewModels;
 using Housing.Selection.Service.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace Housing.Selection.Testing.Service
