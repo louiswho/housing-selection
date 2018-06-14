3f9f995c298858a5d8403291957a8e5037ee30c1
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-14 12:29:44 -0400

    Added HasBedAvailable property to RoomSearchViewModel, Added HasBedAvailableFilter to chain of responsibility, Rebuilt lost FilterFactories, Added new test for BedAvailable

--- src/Housing.Selection.Library/ViewModels/RoomSearchViewModel.cs
+++ src/Housing.Selection.Library/ViewModels/RoomSearchViewModel.cs
@@ -7,5 +7,6 @@
         public double BatchMinimumPercentage { get; set; }
         public string Gender { get; set; }
         public bool IsCompletelyUnassigned { get; set; }
+        public bool HasBedAvailable { get; set; }
     }
 }
