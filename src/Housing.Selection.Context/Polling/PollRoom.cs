39dd76b261d34aafeddd862f2dbba13a7dfcb884
Unknown <mrcrozier@gmail.com>
2018-06-15 16:31:13 -0400

    Added comments to poll classes [#158039176]

--- src/Housing.Selection.Context/Polling/PollRoom.cs
+++ src/Housing.Selection.Context/Polling/PollRoom.cs
@@ -17,6 +17,13 @@ namespace Housing.Selection.Context.Polling
             this.roomRepository = roomRepository;
             this.roomRetrieval = roomRetrieval;
         }
+
+        /// <summary>
+        /// Updates the Rooms in the housing Room database based on the data retrieved from the service hub database
+        /// </summary>
+        /// <returns>
+        /// Returns a Task<List<Room>> that contains the updated Room list
+        /// </returns>
         public async Task<List<Room>> RoomPoll()
         {
             var roomList = new List<Room>();
@@ -31,10 +38,20 @@ namespace Housing.Selection.Context.Polling
             return roomList;
         }
 
-        public Room UpdateRoom(ApiRoom room)
+        /// <summary>
+        /// Updates a single Room in the housing Room database based on the Room data retrieved from the service hub database
+        /// </summary>
+        /// <param name="apiRoom">
+        /// The ApiRoom object retrieved from the RoomRetireval
+        /// Contains the properties to update housing's matching Room with
+        /// </param>
+        /// <returns>
+        /// Returns a Room that contains the updated properties
+        /// </returns>
+        public Room UpdateRoom(ApiRoom apiRoom)
         {
-            var housingRoom = roomRepository.GetRoomByRoomId(room.RoomId);
-            housingRoom = housingRoom.ConvertFromServiceModel(apiRoom: room);
+            var housingRoom = roomRepository.GetRoomByRoomId(apiRoom.RoomId);
+            housingRoom = housingRoom.ConvertFromServiceModel(apiRoom: apiRoom);
             roomRepository.SaveChanges();
             return housingRoom;
         }
