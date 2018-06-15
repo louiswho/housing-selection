6d8f5dcc4edd2a8a7f989df6e049bb0fb41ee83c
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-15 15:54:41 -0400

    Changed property names in SelectionService, Updated SelectionService to utilize service call interfaces, Added Mapper to SelectionService, Added address change with add/remove user, Reimplemented FilterFactories in SelectionService[#158039176]

---
+++ src/Housing.Selection.Context/Selection/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Housing.Selection.Library.HousingModels;
+using Housing.Selection.Library.ServiceHubModels;
+
+namespace Housing.Selection.Context.Selection
+{
+    class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, ApiUser>()
+                .ForMember(d => d.Address, o => o.MapFrom(s => s.Address))
+                .ForMember(d => d.Name, o => o.MapFrom(s => s.Name))
+                .ForMember(d => d.Gender, o=> o.MapFrom(s => s.Gender));
+
+            CreateMap<Room, ApiRoom>()
+                .ForMember(d => d.Address, o => o.MapFrom(s => s.Address));
+
+            CreateMap<Address, ApiAddress>();
+
+            CreateMap<Name, ApiName>();
+        }
+    }
+}
---
+++ src/Housing.Selection.Testing/Context/TempMappingProfileTests.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Housing.Selection.Testing.Context
+{
+    public class TempMappingProfileTests
+    {
+        [Fact]
+        public void MappingProfile_MapsAreValid()
+        {
+            var config = new MapperConfiguration(x => x.AddProfile(new MappingProfile()));
+
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
