9e6ce44f4be331451b16d853f330fc51dd2dc1a1
jalmblade <j.almblade@gmail.com>
2018-06-15 09:51:06 -0400

    Added ServiceHubProxies folder. Added a class for ServiceBatchRetrievalProxy.
    [#158039176]

---
+++ src/Housing.Selection.Context/ServiceHubProxies/ServiceBatchRetrievalProxy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing.Selection.Context.ServiceHubProxies
+{
+    class ServiceBatchRetrievalProxy
+    {
+    }
+}
