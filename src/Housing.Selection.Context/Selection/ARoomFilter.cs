3b95ef922bf578aa8f7415cb1ac2afc0c3317569
Kimberly Waddle <kimberly.waddle@gmail.com>
2018-06-18 10:30:32 -0400

    Moved comments down above the class [#158039176]

--- src/Housing.Selection.Context/Selection/ARoomFilter.cs
+++ src/Housing.Selection.Context/Selection/ARoomFilter.cs
@@ -1,9 +1,10 @@
-﻿/* Chain of responsibility for the room filter custom search. Will parse through a complex object, and filter out rooms based
-   on which feilds are populated (not null). This object is received from the angular API */
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Housing.Selection.Library.HousingModels;
 using Housing.Selection.Library.ViewModels;
+
+/* Chain of responsibility for the room filter custom search. Will parse through a complex object, and filter out rooms based
+   on which feilds are populated (not null). This object is received from the angular API */
 
 namespace Housing.Selection.Context.Selection
 {
